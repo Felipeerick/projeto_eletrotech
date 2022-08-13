6263941bb4e7ef64d2e179f0c3d2a20096a9e0f8
Felipe Erick <rick0531927@gmail.com>
2022-08-13 11:28:31 -0300

    correcao no layout da view errors.not_found

--- projeto-eletrotech/resources/views/errors/not_found.blade.php
+++ projeto-eletrotech/resources/views/errors/not_found.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
 <article class='mt-5 mx-auto text-center'>
-    <h2 class="mb-5">404 | Essa rota não existe ou você não tem acesso!</h2>
     <img src="{{asset('assets/images/not_found.png')}}" width="60%" alt="nof_found">
+    <h4 class="">404 | Essa rota não existe ou você não tem acesso!</h4>
     <div>
         <a href="/" class="btn btn-primary mt-3">Voltar a tela anterior</a>
     </div>
